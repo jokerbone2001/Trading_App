c3fc51d14a3b919feaea27fb938c3c6b4050c540
Marcus Yau <marcus_yau@sfu.ca>
2021-07-07 03:47:34 -0700

    updated docs\readme

--- Code/SFU Circular/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Code/SFU Circular/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,12 +12,13 @@ import android.widget.Toast;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText idTxt,passwordTxt,registerName,registerPass,registerPassConfirm,registerEmail,registerID;
-    Button loginBtn,registerBtn;
-    TextView viewRegisterTxt,viewLoginTxt;
-    String grabUniqueID,grabName,grabLoginId,grabPass,grabPassConfirm,grabEmail;
+    EditText idTxt, passwordTxt, registerName, registerPass,
+            registerPassConfirm, registerEmail, registerID;
+    Button loginBtn, registerBtn;
+    TextView viewRegisterTxt, viewLoginTxt;
+    String grabUniqueID, grabName, grabLoginId, grabPass, grabPassConfirm, grabEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +33,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         viewRegisterTxt = findViewById(R.id.viewRegisterTxt);
 
         registerName = findViewById(R.id.editRegisterName);
-        registerPass=findViewById(R.id.editRPassTxt);
-        registerPassConfirm=findViewById(R.id.editRPassConfirmTxt);
-        registerEmail=findViewById(R.id.editEmailTxt);
-        registerID=findViewById(R.id.editIDTxt);
+        registerPass = findViewById(R.id.editRPassTxt);
+        registerPassConfirm = findViewById(R.id.editRPassConfirmTxt);
+        registerEmail = findViewById(R.id.editEmailTxt);
+        registerID = findViewById(R.id.editIDTxt);
         registerBtn = findViewById(R.id.btnRegister);
-        viewLoginTxt=findViewById(R.id.viewLoginTxt);
+        viewLoginTxt = findViewById(R.id.viewLoginTxt);
 
     }
-    private void registerBoxAppear(){
+
+    private void registerBoxAppear() {
         //make register textbox appear
         registerName.setVisibility(View.VISIBLE);
         registerPass.setVisibility(View.VISIBLE);
@@ -57,7 +59,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         viewRegisterTxt.setVisibility(View.GONE);
 
     }
-    private void loginBoxAppear(){
+
+    private void loginBoxAppear() {
         //make login textbox appear
         idTxt.setVisibility(View.VISIBLE);
         passwordTxt.setVisibility(View.VISIBLE);
@@ -73,53 +76,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         registerBtn.setVisibility(View.GONE);
         viewLoginTxt.setVisibility(View.GONE);
     }
-    private boolean emailRequirement(String email){
-        if(!email.contains("@")||!email.contains(".")){
+
+    private boolean emailRequirement(String email) {
+        if (!email.contains("@") || !email.contains(".")) {
             return false;
         }
         return true;
     }
 
-    private boolean passRequirement(String e1,String e2){
-        boolean hasDigit = false,hasCapital=false,hasLower=false;
-        if(e1.length()<7){ //minimum 8 characters
+    private boolean passRequirement(String e1, String e2) {
+        boolean hasDigit = false, hasCapital = false, hasLower = false;
+        if (e1.length() < 7) { //minimum 8 characters
             return false;
         }
-        if(e1.length()!=e2.length()){ // password matches length of password confirmation
+        if (e1.length() != e2.length()) { // password matches length of password confirmation
             return false;
         }
-        for(int i=0;i<e1.length();i++){
+        for (int i = 0; i < e1.length(); i++) {
             char ch = e1.charAt(i);
-            if(Character.isDigit(ch)){ //has a digit
-                hasDigit=true;
-            }else if(Character.isUpperCase(ch)){ //has capital
-                hasCapital=true;
-            }else if(Character.isLowerCase(ch)){ //has lower
-                hasLower=true;
+            if (Character.isDigit(ch)) { //has a digit
+                hasDigit = true;
+            } else if (Character.isUpperCase(ch)) { //has capital
+                hasCapital = true;
+            } else if (Character.isLowerCase(ch)) { //has lower
+                hasLower = true;
             }
         }
-        if(!hasDigit||!hasCapital||!hasLower){ //failed to meet requirement of for-loop
+        if (!hasDigit || !hasCapital || !hasLower) { //failed to meet requirement of for-loop
             return false;
         }
         return true; // meets all requirements
     }
-    private boolean passMatch(String e1,String e2){
-        boolean matches=false;
-        for(int i=0;i<e1.length();i++){
-            if(e1.charAt(i)==e2.charAt(i)){
-                matches=true;
+
+    private boolean passMatch(String e1, String e2) {
+        boolean matches = false;
+        for (int i = 0; i < e1.length(); i++) {
+            if (e1.charAt(i) == e2.charAt(i)) {
+                matches = true;
             }
         }
         //failed to meet requirement of for-loop
         return matches;
     }
-    private boolean loginIDRequirement(String ID){
-        if(ID.length()!=9){ // has to be 9 characters long
+
+    private boolean loginIDRequirement(String ID) {
+        if (ID.length() != 9) { // has to be 9 characters long
             return false;
         }
-        for(int i =0;i<ID.length();i++){
+        for (int i = 0; i < ID.length(); i++) {
             char ch = ID.charAt(i);
-            if(!Character.isDigit(ch)){ // check if there are any letters in ID (number only)
+            if (!Character.isDigit(ch)) { // check if there are any letters in ID (number only)
                 return false;
             }
         }
@@ -128,78 +134,85 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        DBUserHelper dbHelper = new DBUserHelper(MainActivity.this); //DBUserHelper class, see DBUserHelper for more detail
-        UserClass userClass; //Creating a UserClass variable called userClass, see UserClass class for more detail
-        List<UserClass> usersList = dbHelper.getUserInfo();// grabs all existing user info from user database - See DBUserHelper class for more info
-        switch(v.getId()){
+        //DBUserHelper class, see DBUserHelper for more detail
+        DBUserHelper dbHelper = new DBUserHelper(MainActivity.this);
+        //Creating a UserClass variable called userClass, see UserClass class for more detail
+        UserClass userClass;
+        // grabs all existing user info from user database - See DBUserHelper class for more info
+        List<UserClass> usersList = dbHelper.getUserInfo();
+        switch (v.getId()) {
             case R.id.btnLogin: // actions executed when clicked on the "login button"
-                grabLoginId=idTxt.getText().toString().trim();
-                grabPass=passwordTxt.getText().toString().trim();
-                if(grabLoginId.isEmpty()){ // checks if idTxt is empty
-                    idTxt.setError("Field required"); //setError pops a warning msg at the error location
+                grabLoginId = idTxt.getText().toString().trim();
+                grabPass = passwordTxt.getText().toString().trim();
+                if (grabLoginId.isEmpty()) { // checks if idTxt is empty
+                    //setError pops a warning msg at the error location
+                    idTxt.setError("Field required");
                     idTxt.requestFocus();  //focuses cursor at error location
                     return;
                 }
-                if(grabPass.isEmpty()){ //checks if passwordTxt is empty
+                if (grabPass.isEmpty()) { //checks if passwordTxt is empty
                     passwordTxt.setError("Field required");
                     passwordTxt.requestFocus();
                     return;
                 }
-                boolean errorToast=true;
-                for(UserClass x : usersList){ //loops through userList to find if there are such registered user info.
-                    if(x.getLoginID().equals(grabLoginId)&&x.getPassword().equals(grabPass)){
+                boolean errorToast = true;
+                //loops through userList to find if there are such registered user info.
+                for (UserClass x : usersList) {
+                    if (x.getLoginID().equals(grabLoginId) && x.getPassword().equals(grabPass)) {
                         //login go to next activity
                         finish(); //closes current activity
-                        Intent intent = new Intent(MainActivity.this,HomePage.class); // from MainActivity screen to HomePage screen
+                        // from MainActivity screen to HomePage screen
+                        Intent intent = new Intent(MainActivity.this, HomePage.class);
                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                         /*
                         .putExtra("key",data(int,String,etc...)
                         essentially passing data from one screen to the next screen
                         see HomePage.java under "onCreate" for more info
                          */
-                        intent.putExtra("UNIQUEID",x.getUniqueID());
-                        intent.putExtra("LOGINID",x.getLoginID());
-                        intent.putExtra("NAME",x.getName());
-                        intent.putExtra("EMAIL",x.getEmail());
+                        intent.putExtra("UNIQUEID", x.getUniqueID());
+                        intent.putExtra("LOGINID", x.getLoginID());
+                        intent.putExtra("NAME", x.getName());
+                        intent.putExtra("EMAIL", x.getEmail());
                         startActivity(intent); // starts the next screen
-                        Toast.makeText(MainActivity.this,"Logging in...",Toast.LENGTH_SHORT).show(); // pops a helpful msg
-                        errorToast=false;
+                        // pops a helpful msg
+                        Toast.makeText(MainActivity.this, "Logging in...", Toast.LENGTH_SHORT).show();
+                        errorToast = false;
                     }
                 }
-                if(errorToast){ // if no such user exists from provided info then pop useful msg
-                    Toast.makeText(MainActivity.this,"Either user doesn't exist or info entered is wrong",Toast.LENGTH_SHORT).show();
+                if (errorToast) { // if no such user exists from provided info then pop useful msg
+                    Toast.makeText(MainActivity.this, "Either user doesn't exist or info entered is wrong", Toast.LENGTH_SHORT).show();
                 }
                 break;
             case R.id.btnRegister:// actions executed when clicked on the "register button"
                 boolean noDupe = true;
                 //grabs the string for user and password
-                grabName=registerName.getText().toString().trim();
-                grabLoginId=registerID.getText().toString().trim();
-                grabPass=registerPass.getText().toString().trim();
-                grabPassConfirm=registerPassConfirm.getText().toString().trim();
-                grabEmail=registerEmail.getText().toString().trim();
-                try{
+                grabName = registerName.getText().toString().trim();
+                grabLoginId = registerID.getText().toString().trim();
+                grabPass = registerPass.getText().toString().trim();
+                grabPassConfirm = registerPassConfirm.getText().toString().trim();
+                grabEmail = registerEmail.getText().toString().trim();
+                try {
 
-                    if(grabName.isEmpty()){
+                    if (grabName.isEmpty()) {
                         registerName.setError("Enter your name");
                         registerName.requestFocus();
                         return;
                     }
-                    if(!loginIDRequirement(grabLoginId)){
+                    if (!loginIDRequirement(grabLoginId)) {
                         registerID.setError("Requirements: 9 digits");
                         registerID.requestFocus();
                         return;
                     }
-                    if(!passRequirement(grabPass,grabPassConfirm)){
+                    if (!passRequirement(grabPass, grabPassConfirm)) {
                         registerPass.setError("Requirements: 1 Upper, 1 Lower, 1 Digit, Minimum 8 characters");
                         registerPass.requestFocus();
                         return;
-                    }else if(!passMatch(grabPass,grabPassConfirm)){
+                    } else if (!passMatch(grabPass, grabPassConfirm)) {
                         registerPassConfirm.setError("Passwords does not match");
                         registerPassConfirm.requestFocus();
                         return;
                     }
-                    if(!emailRequirement(grabEmail)){
+                    if (!emailRequirement(grabEmail)) {
                         registerEmail.setError("Enter proper email, must contain '@' and '.'");
                         registerEmail.requestFocus();
                         return;
@@ -208,7 +221,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     //Implement code to check existing users so it doesn't create duplicates
                     //...
                     //loops through list of users to check duplicate
-                    for(UserClass x : usersList){
+                    for (UserClass x : usersList) {
                         //debug purposes
 //                        System.out.println("unique ID "+x.getUniqueID());
 //                        System.out.println("name ID "+x.getName());
@@ -223,18 +236,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         }
                     }
                     //sets the userClass with         -1 Name,    loginID,    password, email
-                    userClass = new UserClass(-1,grabName,grabLoginId,grabPass,grabEmail);
+                    userClass = new UserClass(-1, grabName, grabLoginId, grabPass, grabEmail);
                     //...
-                }catch(Exception e){
+                } catch (Exception e) {
                     //error catching
-                    userClass = new UserClass(-1,"Placeholder Error Name","Placeholder Error Login","Placeholder Error Pass","Placeholder Error Email");
-                    Toast.makeText(MainActivity.this,"User NOT Created",Toast.LENGTH_SHORT).show();
+                    userClass = new UserClass(-1, "Placeholder Error Name", "Placeholder Error Login", "Placeholder Error Pass", "Placeholder Error Email");
+                    Toast.makeText(MainActivity.this, "User NOT Created", Toast.LENGTH_SHORT).show();
                 }
-                if(noDupe){
+                if (noDupe) {
                     // if noDupe is true then add user to 'User Database'
                     dbHelper.add(userClass);
                     //pops a little message at bottom of screen to let user know it has been created
-                    Toast.makeText(MainActivity.this,"User Created",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "User Created", Toast.LENGTH_SHORT).show();
                     registerName.setText("");
                     registerID.setText("");
                     registerPass.setText("");
@@ -242,9 +255,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     registerEmail.setText("");
                     //makes login options appear
                     loginBoxAppear();
-                }else{
+                } else {
                     //if dupe then pop a helpful message
-                    Toast.makeText(MainActivity.this,"Account already exists",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Account already exists", Toast.LENGTH_SHORT).show();
                 }
                 break;
             case R.id.viewLoginTxt:
